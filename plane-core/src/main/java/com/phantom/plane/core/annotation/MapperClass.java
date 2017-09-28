0d1abd2ba939c1e1861c34c4e01b6cd9300500e4
zrarzw <zrarzw@163.com>
2017-09-28 15:30:29 +0800

    Signed-off-by: zrarzw <zrarzw@163.com>

---
+++ plane-core/src/main/java/com/phantom/plane/core/annotation/MapperClass.java
@@ -0,0 +1,21 @@
+
+package com.phantom.plane.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Athos on2016-09-04  .6767颠三倒四
+ */
+@SuppressWarnings("unchecked")
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MapperClass {
+	/** 
+	 *指定 MapperClass
+	 */
+    Class value();
+}
+
---
+++ plane-core/src/main/java/com/phantom/plane/core/base/BaseEntity.java
@@ -0,0 +1,71 @@
+
+package com.phantom.plane.core.base;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import tk.mybatis.mapper.annotation.NameStyle;
+import tk.mybatis.mapper.code.Style;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@NameStyle(value = Style.camelhumpAndLowercase)
+public class BaseEntity implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("id")
+	private Integer id;
+
+	/**
+	 *  创建时间1111
+	 */
+	@Column 
+	private Date createTime;
+
+	/**
+	 * 创建人
+	 */
+	@Column
+	private String createUser;
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	/**
+	 * @return the createUser
+	 */
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	/**
+	 * @param createUser
+	 *            the createUser to set
+	 */
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+}
+
